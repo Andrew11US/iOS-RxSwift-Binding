5f9fb8e034a6c490600312446390d3e787d5eed7
Andrii Halabuda <andrii.halabuda@gmail.com>
2020-09-06 15:22:41 +0200

    Added Rx pods and view with gesture recognizer

--- RxSwift Binding/ViewController.swift
+++ RxSwift Binding/ViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var circleView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setup()
+    }
+    
+    func setup() {
+        circleView = UIView(frame: CGRect(origin: view.center, size: CGSize(width: 100.0, height: 100.0)))
+        circleView.layer.cornerRadius = circleView.frame.width / 2.0
+        circleView.center = view.center
+        circleView.backgroundColor = .purple
+        view.addSubview(circleView)
+        
+        // Add gesture recognizer
+        let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(circleMoved(_:)))
+        circleView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    
+     
+    @objc func circleMoved(_ recognizer: UIPanGestureRecognizer) {
+        let location = recognizer.location(in: view)
+        UIView.animate(withDuration: 0.1) {
+            self.circleView.center = location
+        }
     }
 
 
